c43ee0e6a065b84fff68dbd73dfda6ad2af2a9db
Pauljosueee <166031203+Pauljosueee@users.noreply.github.com>
2024-05-15 07:59:10 -0500

    Add files via upload

---
+++ 11-funciones.js
@@ -0,0 +1,20 @@
+'use strict'
+
+function mostrarMensaje(){
+    alert("hola")
+}
+//mostrarMensaje();
+
+function mostrarMensajeConNombre(nombre){
+    alert("Hola"+nombre)
+}
+//mostrarMensajeConNombre(" Juan");
+
+function GenerarFrase (){
+    return "Hola Mundo"; 
+}
+alert (GenerarFrase ());
+
+function sumar ..(){
+
+}
